a43791dd57f8dbd2ab8dc2d03880e8044b089ecb
talhaw-cmd <123mtalha789@gmail.com>
2025-05-18 23:04:54 +0500

    first commit

--- app/page.js
+++ app/page.js
@@ -1,103 +1,89 @@
-import Image from "next/image";
+"use client"
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export default function Home() {
+const page = () => {
+  const [first, setfirst] = useState("")
+  const [book, setbook] = useState("")
+  const [number, setnumber] = useState("")
+  const [status, setstatus] = useState("")
+  const [urdu, seturdu] = useState("")
+  const [arabic, setarabic] = useState("")
+  const [chapter, setchapter] = useState("")
+   const [loading, setLoading] = useState(true);
+
+  const generate = async()=>{
+    let page = 1179;
+    let total = 29465;
+    let perpage = 25;
+  let rpage = Math.floor(Math.random() * page);
+  let random = Math.floor(Math.random() * perpage);
+
+ 
+const apiUrl = `https://hadithapi.com/api/hadiths?page=${rpage}&apiKey=$2y$10$K5c4MswceLti9g4Gz0wZCaFgoMv4OFKLQvuWVTvctsOEvaNUvc2`;
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
+    setLoading(false);
+
+   const main = data.hadiths.data[random]
+   const hbook = main.book.bookName
+   const hnum = main.hadithNumber
+   const hurdu = main.hadithUrdu
+   const harabic = main.hadithArabic
+   const hstatus = main.status
+   const hnarrator = main.urduNarrator
+   const hchapter = main.chapter.chapterArabic
+   setbook(hbook)
+   setnumber(hnum)
+   setarabic(harabic)
+   seturdu(hurdu)
+   setstatus(hstatus)
+   setchapter(hchapter)
+   console.log(main)
+
+})
+.catch(error => {
+    console.log(error);
+})
+
+setLoading(true);
+  }
+
+  useEffect(() => {
+    generate();
+  }, [])
+  
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+ <div className="p-5 gap-2 flex flex-col justify-start items-center w-full">
+      <div className="w-full flex flex-col justify-center items-center rounded-lg text-white bg-gray-800 py-4 px-6">
+        <h2 className="text-gray-500">BookName</h2>
+        <h2 className="text-xl">{book}</h2>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className="w-full flex gap-1 flex-col justify-center items-center rounded-lg text-white bg-gray-800 py-4 px-6">
+        <h1 className="text-gray-500">Hadith</h1>
+        <h1 className="text-xl text-end leading-normal">{arabic}</h1>
+        <h1 className="text-gray-500 mt-2">Tarjuma</h1>
+        <h1 className="text-2xl text-end leading-normal">{urdu}</h1>
+      </div>
+
+      <div className="w-full flex flex-row justify-around items-center rounded-lg text-white bg-gray-800 py-4 px-6">
+        <div className="flex gap-2"><h2 className="text-gray-500">HadithNumber:</h2><h2>{number}</h2></div>      
+        <div className="flex gap-2"><h2 className="text-gray-500">Status:</h2><h2>{status}</h2></div>
+        <div className="flex gap-2"><h2 className="text-gray-500">Chapter:</h2><h2>{chapter}</h2></div>
+      </div>
+
+      <button 
+        onClick={generate} 
+        disabled={loading}
+        className="bg-gray-600 px-4 py-4 text-lg w-full text-center rounded-lg text-white hover:cursor-pointer disabled:opacity-50"
+      >
+        {loading ? "Loading..." : "Generate Hadith"}
+      </button>
     </div>
-  );
+  )
 }
+
+export default page
